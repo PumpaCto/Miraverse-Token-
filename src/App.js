14563063466007a47d2f79ee28bcec6f27f95709
PumpaCto <pumpa6827@gmail.com>
2025-04-27 21:20:43 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Roadmap from "./pages/Roadmap";
+import Community from "./pages/Community";
+import FAQ from "./pages/FAQ";
+import Gallery from "./pages/Gallery";
+import Tokenomics from "./pages/Tokenomics";
+import Partners from "./pages/Partners";
+import Newsletter from "./pages/Newsletter";
+import Lore from "./pages/Lore";
+import Team from "./pages/Team";
+import OpenWorld from "./pages/OpenWorld";
+import PlayToEarn from "./pages/PlayToEarn";
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-black text-white min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/roadmap" element={<Roadmap />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/tokenomics" element={<Tokenomics />} />
+            <Route path="/partners" element={<Partners />} />
+            <Route path="/newsletter" element={<Newsletter />} />
+            <Route path="/lore" element={<Lore />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/openworld" element={<OpenWorld />} />
+            <Route path="/playtoearn" element={<PlayToEarn />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
